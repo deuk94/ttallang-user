64edf3c00b654697ce420cad3e3e77894dc555e9
mhd329 <mhd329@gmail.com>
2024-11-04 03:29:09 +0900

    주말 작업 테스트 완료

--- src/main/java/com/ttallang/user/ServletInitializer.java
+++ src/main/java/com/ttallang/user/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TtallangUserApplication.class);
+        return application.sources(TtallangApplication.class);
     }
 
 }
