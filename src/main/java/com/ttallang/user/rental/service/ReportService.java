2782c1bba1b2d3566209ad867ddf54818fdccba8
DESKTOP-4U4G4G2\KOSA <koxn1974@naver.com>
2024-11-14 20:10:09 +0900

    신고 멘트 수정

--- src/main/java/com/ttallang/user/rental/service/ReportService.java
+++ src/main/java/com/ttallang/user/rental/service/ReportService.java
@@ -170,7 +170,7 @@ public class ReportService {
             result.put("msg", "신고 및 자전거 반납이 완료되었습니다. 이용 시간에 따른 결제가 필요합니다. 결제 페이지로 이동합니다.");
             paymentsService.calculateAndSavePayment(rental.getRentalId(), customerId);
         } else {
-            result.put("msg", "신고 및 자전거 반납이 성공적으로 완료되었습니다. 추가 결제는 필요하지 않습니다.");
+            result.put("msg", "신고 및 자전거 반납이 성공적으로 완료되었습니다. 5분 이내 반납으로 이용 요금이 전혀 발생하지 않습니다.");
         }
 
         result.put("report", faultReport);
